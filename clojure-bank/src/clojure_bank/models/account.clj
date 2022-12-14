b0ee0bf68055f277ee4437ef1a6cbd46fba0f831
Cezar Cruz <cezar.cruz@picpay.com>
2022-12-14 09:41:38 -0300

    removing clojure

--- clojure-bank/src/clojure_bank/models/account.clj
+++
@@ -1,6 +0,0 @@
-(ns clojure-bank.models.account
-  (:require [schema.core :as s]))
-
-(s/defschema Account {:account/number s/Uuid
-                      :account/agency s/Str
-                      :account/balance s/Num})
--- clojure-bank/src/clojure_bank/wire/in/account.clj
+++
@@ -1,5 +0,0 @@
-(ns clojure-bank.wire.in.account
-  (:require [schema.core :as s]))
-
-(s/defschema AccountCreationRequest {(s/optional-key :account-number) s/Uuid
-                                     (s/required-key :agency)         s/Str})
