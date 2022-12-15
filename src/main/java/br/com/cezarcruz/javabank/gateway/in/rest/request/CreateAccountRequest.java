7cb46a2e73941bc4b0cd0bad82c881bb9a3fe9d4
Cezar Cruz <cezarcruz@outlook.com>
2022-12-15 11:40:40 -0300

    moving to gradle

--- src/main/java/br/com/cezarcruz/javabank/gateway/in/rest/request/CreateAccountRequest.java
+++ src/main/java/br/com/cezarcruz/javabank/gateway/in/rest/request/CreateAccountRequest.java
@@ -1,6 +1,5 @@
 package br.com.cezarcruz.javabank.gateway.in.rest.request;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,9 +10,8 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateAccountRequest {
-    @NotBlank
+
     private String agency;
 
-    @NotBlank
     private String document;
 }
