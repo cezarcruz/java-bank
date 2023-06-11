0d33a37fa5dcaaeb4090abe841f1c1ed46caf5a9
Cezar Cruz <cezarcruz@outlook.com>
2023-06-11 11:51:04 -0300

    removing redis tail

--- src/test/java/br/com/cezarcruz/javabank/gateway/out/schedule/UpdateCacheSchedulerTest.java
+++ src/test/java/br/com/cezarcruz/javabank/gateway/out/schedule/UpdateCacheSchedulerTest.java
@@ -1,31 +1,29 @@
 package br.com.cezarcruz.javabank.gateway.out.schedule;
 
-import br.com.cezarcruz.javabank.gateway.out.cache.redis.RedisCacheWrapper;
+import br.com.cezarcruz.javabank.gateway.out.cache.Cache;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class UpdateCacheSchedulerTest {
 
-    @Mock
-    private RedisCacheWrapper redisCache;
+    @Spy
+    private Cache cache = new Cache() {
+        @Override
+        public void put(String key) {
+            System.out.println(key);
+        }
+    };
 
     @InjectMocks
     private UpdateCacheScheduler updateCacheScheduler;
 
     @Test
     void justRun() {
-        doNothing().when(redisCache).put(any());
         updateCacheScheduler.run();
-
-        verify(redisCache).put(any());
     }
 
 }
